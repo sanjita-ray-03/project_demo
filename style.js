4ecd64ca6b187345bed59ae9093158c92b057bfc
Sanjita Ray <sanjitaray62@gmail.com>
2025-03-02 00:23:47 +0530

    edited

--- style.js
+++ style.js
@@ -18,3 +18,14 @@
       })
     }, false)
   }())
+
+
+
+
+
+// const myCarouselElement = document.querySelector('#myCarousel')
+
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   interval: 2000,
+//   touch: false
+// })
